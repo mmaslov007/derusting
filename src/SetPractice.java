36a80cabe862ad06216c069f2956ac0e079d7902
Maxwell Maslov <Max>
2025-01-02 21:23:37 -0800

    Completed SetPractice.

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -1,17 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetPractice {
   public static void main(String[] args) {
     // Create a HashSet of Strings and assign it to a variable of type Set
+    Set<String> hashSet = new HashSet<String>();
 
     // Add 3 elements to the set
     // (It's OK to do it one-by-one)
+    hashSet.add("Monolith");
+    hashSet.add("Freedom");
+    hashSet.add("Duty");
+    System.out.println(hashSet + "\n");
 
     // Check whether the Set contains a given String
+    System.out.println(hashSet.contains("Freedom") + "\n");
 
     // Remove an element from the Set
+    hashSet.remove("Freedom");
 
     // Get the size of the Set
+    System.out.println(hashSet.size() + "\n");
 
     // Iterate over the elements of the Set, printing each one on a separate line
+    for (String string : hashSet) {
+      System.out.println(string);
+    }
 
     /*
      * Warning!
