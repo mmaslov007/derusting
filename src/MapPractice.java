68589b4a4dca4bcd901953fa83fb3548e042792e
Maxwell Maslov <Max>
2025-01-02 21:43:48 -0800

    Completed MapPractice.

--- src/MapPractice.java
+++ src/MapPractice.java
@@ -1,28 +1,51 @@
-
+import java.util.Map;
+import java.util.HashMap;
 
 public class MapPractice {
   public static void main(String[] args) {
     // Create a HashMap with String keys and Integer values and
     // assign it to a variable of type Map
+    Map<String, Integer> hashMap = new HashMap<String, Integer>();
 
     // Put 3 different key/value pairs in the Map
     // (it's OK to do this one-by-one)
-
+    hashMap.put("Monolith", 1);
+    hashMap.put("Freedom", 2);
+    hashMap.put("Duty", 3);
+    System.out.println(hashMap + "\n");
+    
     // Get the value associated with a given key in the Map
+    System.out.println(hashMap.get("Monolith") + "\n");
 
     // Find the size (number of key/value pairs) of the Map
+    System.out.println(hashMap.size() + "\n");
 
-    // Replace the value associated with a given key (the size of the Map shoukld not change)
+    // Replace the value associated with a given key (the size of the Map should not change)
+    hashMap.put("Monolith", 4);
+    System.out.println(hashMap + "\n");
 
     // Check whether the Map contains a given key
+    System.out.println(hashMap.containsKey("Freedom") + "\n");
 
     // Check whether the Map contains a given value
+    System.out.println(hashMap.containsValue(4) + "\n");
 
     // Iterate over the keys of the Map, printing each key
+    for (String key : hashMap.keySet()) {
+      System.out.println(key);
+    }
+    System.out.println("\n");
 
     // Iterate over the values of the map, printing each value
+    for (Integer value : hashMap.values()) {
+      System.out.println(value);
+    }
+    System.out.println("\n");
 
     // Iterate over the entries in the map, printing each key and value
+    for (String key : hashMap.keySet()) {
+      System.out.println(key + ", " + hashMap.get(key));
+    }
 
     /*
      * Usage tip!
